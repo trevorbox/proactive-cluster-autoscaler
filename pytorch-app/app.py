fee24c08c7336cfe40e191550abeef142e544579
tbox <tbox@redhat.com>
2021-04-05 14:08:08 -0700

    Entitlements

--- pytorch-app/app.py
+++ pytorch-app/app.py
@@ -2,18 +2,18 @@ import torch
 import time
 import nvsmi
 
-try:
-  nvsmi.get_gpus()
-  nvsmi.get_available_gpus()
-  nvsmi.get_gpu_processes()
-except :
-  print("No nvidia-smi (or other error)...")
-
-x = torch.rand(5, 3)
-print(x)
-
-print("Cuda is available: " + str(torch.cuda.is_available()))
-
-print("Sleep...")
 while True:
-  time.sleep(1)
+
+  print(torch.rand(5, 3))
+
+  try:
+    nvsmi.get_gpus()
+    nvsmi.get_available_gpus()
+    nvsmi.get_gpu_processes()
+  except :
+    print("No nvidia-smi (or other error)...")
+  
+  print("Cuda is available: " + str(torch.cuda.is_available()))
+  
+  print("Sleep for 60 seconds...")
+  time.sleep(60)
