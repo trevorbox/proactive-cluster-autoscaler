9ac2bba673f0f836e17851788b139db51ac6e9b3
tbox <tbox@redhat.com>
2021-04-05 15:29:55 -0700

    randn with cuda

--- pytorch-app/app.py
+++ pytorch-app/app.py
@@ -4,14 +4,12 @@ import os
 
 while True:
 
-  print(torch.rand(5, 3))
-
   try:
     os.system('nvidia-smi')
+    print("Cuda is available: " + str(torch.cuda.is_available()))
+    print(torch.randn(10240, 10240, dtype=torch.double, device='cuda'))
   except :
     print("No nvidia-smi (or other error)...")
-  
-  print("Cuda is available: " + str(torch.cuda.is_available()))
-  
+    
   print("Sleep for 60 seconds...")
   time.sleep(60)
